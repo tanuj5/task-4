5c695bb6b928407bf0b12ab8858105cfc0f7d350
tanuj5 <43009339+tanuj5@users.noreply.github.com>
2020-07-22 01:17:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,17 @@
  <head>
   <title>PHP Test</title>
  </head>
- <body>
- <?php echo '<p>Welcome to my website.</p>' ?> 
+ <body bgcolor=aqua>
+  <font color=red>
+   <h1> Hello! </h1>
+   <br>
+   <br>
+   <h1> Hope you are doing fine! <h1>
+    <br>
+    <br>
+    </font>
+ <?php
+  echo "HAVE A GOOD DAY" ;
+  ?> 
  </body>
 </html>
